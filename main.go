fb5602c039212b885fc2a6f2a0bf613d4b9838de
Gabor Tanz <gabor.tanz@gmail.com>
2017-04-16 09:11:28 +0200

    fix ip parsing

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ import (
 )
 
 type DHTResponse map[string]interface{}
-type NodeInfo string
+
+type NodeInfo struct {
+	id   string
+	addr *net.UDPAddr
+}
 
 func CheckError(err error) {
 	if err != nil {
@@ -42,25 +46,37 @@ func findNodesQuery(id string) []byte {
 	return bencode.Encode(q)
 }
 
-func (n NodeInfo) String() string {
-	ip := net.IPv4(n[0], n[1], n[2], n[3])
-	port := binary.BigEndian.Uint16([]byte(n[4:]))
-	return fmt.Sprintf("%v:%d", ip, port)
+//func (n NodeAddr) String() string {
+//	ip := net.IPv4(n[0], n[1], n[2], n[3])
+//	port := binary.BigEndian.Uint16([]byte(n[4:]))
+//	return fmt.Sprintf("%v:%d", ip, port)
+//}
+
+func parseIP(addr string) (*net.UDPAddr, error) {
+	ip := net.IPv4(addr[0], addr[1], addr[2], addr[3])
+	port := binary.BigEndian.Uint16([]byte(addr[4:]))
+
+	return net.ResolveUDPAddr("udp", fmt.Sprintf("%v:%d", ip, port))
 }
 
 func (d DHTResponse) String() string {
-	node := NodeInfo(d["ip"].(string))
+	nodeAddr, _ := parseIP(d["ip"].(string))
 	tx := d["t"].(string)
 	r := d["r"].(map[string]interface{})
 	id := r["id"].(string)
 	n := r["nodes"].(string)
 	nodes := []NodeInfo{}
 	for i := 0; i < len(n); {
-		nodes = append(nodes, NodeInfo(n[i:i+26]))
+		id := n[i : i+20]
+		addr, _ := parseIP(n[i+20 : i+26])
+		node := NodeInfo{id, addr}
+		fmt.Printf("%s %x\n", addr, id)
 		i = i + 26
+		nodes = append(nodes, node)
 	}
 
-	return fmt.Sprintf("Receiving: %s TX: %x ID: %x\nNodes: %v", node, tx, id, nodes)
+	//return fmt.Sprintf("Receiving: TX: %x ID: %x\nNodes: %v", tx, id, nodes)
+	return fmt.Sprintf("Receiving: %s TX: %x ID: %x\n", nodeAddr, tx, id)
 }
 
 func main() {
